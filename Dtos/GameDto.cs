bb3dc69742408dfecca049b9ae12347dd6002121
fernandobouchet <fernandobouchet@gmail.com>
2024-07-29 22:28:51 -0300

    feat: add initial DTO and data examples

---
+++ Dtos/GameDto.cs
@@ -0,0 +1,3 @@
+﻿namespace gameStoreApi;
+
+public record class GameDto(int Id, string Name, string Genre, decimal Price, DateOnly ReleaseDate);
--- Program.cs
+++ Program.cs
@@ -1,5 +1,16 @@
+using gameStoreApi;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+List<GameDto> games =
+[
+    new(1, "Street Fighter II", "Figthing", 19.99M, new DateOnly(1992, 7, 15)),
+    new(2, "Final Fantasy XIV", "RolePLaying", 59.99M, new DateOnly(2010, 9, 30)),
+    new(3, "FIFA 23", "Sports", 69.99M, new DateOnly(2022, 9, 27))
+];
+
+app.MapGet("games", () => games);
 
 app.MapGet("/", () => "Hello World!");
 
