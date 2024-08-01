c73f15a583adbb1e03a5d15a54f08f4391f98b79
fernandobouchet <fernandobouchet@gmail.com>
2024-07-31 22:41:56 -0300

    feat: create migration for seed data

--- Data/Migrations/20240801000536_SeegGenres.Designer.cs
+++ Data/Migrations/20240801000536_SeegGenres.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using gameStoreApi.Data;
 
@@ -10,9 +11,11 @@ using gameStoreApi.Data;
 namespace gameStoreApi.Data.Migrations
 {
     [DbContext(typeof(GameStoreContext))]
-    partial class GameStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20240801000536_SeegGenres")]
+    partial class SeegGenres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.7");
